2b8019c271b6bc9318ed44e87ab569d7df1fc988
SoulShakti <soulshaktie@gmail.com>
2025-10-14 20:30:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: ['https://soulshaktiwellness.com', 'https://www.soulshaktiwellness.com', 'http://localhost:3000'],
   credentials: true
 }));
 app.use(express.json());
